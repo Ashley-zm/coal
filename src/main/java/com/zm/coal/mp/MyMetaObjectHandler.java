8918032c38efd6b0401bd94562a7c7960c56c51e
朱梅 <1540588534@qq.com>
2021-01-09 10:29:16 +0800

    login-common.js

---
+++ src/main/java/com/zm/coal/mp/MyMetaObjectHandler.java
@@ -0,0 +1,9 @@
+package com.zm.coal.mp;
+
+/**
+ * @Author ZhuMei
+ * @Date 2021/1/8 21:38
+ * @Version 1.0
+ */
+public class MyMetaObjectHandler {
+}
---
+++ src/main/java/com/zm/coal/util/ResultUtil.java
@@ -0,0 +1,9 @@
+package com.zm.coal.util;
+
+/**
+ * @Author ZhuMei
+ * @Date 2021/1/7 21:43
+ * @Version 1.0
+ */
+public class ResultUtil {
+}
