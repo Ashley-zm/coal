e4f3a2c41c3b89ba4cf77a189f78fe0c5d2dc6e9
朱梅 <1540588534@qq.com>
2021-01-09 16:49:56 +0800

    MybatisPlusConfig

--- src/main/java/com/zm/coal/config/MybatisPlusConfig.java
+++ src/main/java/com/zm/coal/config/MybatisPlusConfig.java
@@ -4,15 +4,19 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Author ZhuMei
  * @Date 2021/1/4 20:23
  * @Version 1.0
  */
+@Configuration
 public class MybatisPlusConfig {
     /**
-     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
@@ -22,4 +26,6 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
+
+
 }
---
+++ src/main/java/com/zm/coal/query/AccountQuery.java
@@ -0,0 +1,9 @@
+package com.zm.coal.query;
+
+/**
+ * @Author ZhuMei
+ * @Date 2021/1/9 14:42
+ * @Version 1.0
+ */
+public class AccountQuery {
+}
