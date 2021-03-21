16225556d2e644d2a48f775b9b6f16c85c5a1923
朱梅 <1540588534@qq.com>
2021-03-21 21:22:47 +0800

    update all the pages

--- src/main/resources/static/js/my/my.js
+++
@@ -1,9 +0,0 @@
-
-$.get('/my/toDetail/' + localStorage.getItem("accountId"), function (res) {
-    layer.open({
-        type: 1,
-        title: title,
-        area: ['800px', '450px'],//宽高
-        content: res
-    });
-});
